8750fa23d7ab4c9728a4044a349dccddf7110087
José Andrés Abad. <64880801+JAAC8@users.noreply.github.com>
2020-07-14 20:01:59 -0500

    Actualización

---
+++ src/main/java/ec/edu/ec/controlador/ControladorCliente.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.ec.controlador;
+
+import ec.edu.ec.dao.DAOClienteI;
+import ec.edu.ec.dao.DAOCliente;
+
+/**
+ *
+ * @author José Andrés Abad
+ */
+public class ControladorCliente {
+    private DAOClienteI dao;
+    public ControladorCliente(){
+        dao = new DAOCliente();
+    }
+    public void crear(){
+        
+    }
+    
+    public void leer(){
+        
+    }
+    
+    public void actualizar(){
+        
+    }
+    
+    public void eliminar(){
+        
+    }
+}
