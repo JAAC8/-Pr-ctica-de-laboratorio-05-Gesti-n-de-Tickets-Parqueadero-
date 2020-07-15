ab78b70045a4e5cea977c46ad4b148671a5af7ac
José Andrés Abad. <64880801+JAAC8@users.noreply.github.com>
2020-07-15 08:13:17 -0500

    Actualización

--- src/main/java/ec/edu/ec/controlador/ControladorCliente.java
+++ src/main/java/ec/edu/ec/controlador/ControladorCliente.java
@@ -13,23 +13,30 @@ import ec.edu.ec.dao.DAOCliente;
  * @author José Andrés Abad
  */
 public class ControladorCliente {
+
     private DAOClienteI dao;
-    public ControladorCliente(){
-        dao = new DAOCliente();
+    private ControladorTicket cT;
+    private ControladorVehiculo cV;
+
+    public ControladorCliente(DAOCliente dao, ControladorTicket cT, ControladorVehiculo cV) {
+        this.dao = dao;
+        this.cT = cT;
+        this.cV = cV;
     }
-    public void crear(){
-        
+
+    public void crear() {
+
     }
-    
-    public void leer(){
-        
+
+    public void leer() {
+
     }
-    
-    public void actualizar(){
-        
+
+    public void actualizar() {
+
     }
-    
-    public void eliminar(){
-        
+
+    public void eliminar() {
+
     }
 }
